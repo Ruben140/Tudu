65585a3242f97cf8d424507e76160cca49ff3185
ruben140 <ruben140@icloud.com>
2024-09-16 11:17:13 +0200

    Initial push of empty XCode project

---
+++ Tudu/TuduApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuduApp.swift
+//  Tudu
+//
+//  Created by Ruben de Koning on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TuduApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
