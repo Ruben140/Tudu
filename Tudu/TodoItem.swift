0351656b3d3dbc59e89b3b330d0878de8085b861
ruben140 <ruben140@icloud.com>
2024-09-16 11:21:31 +0200

    Created task model

---
+++ Tudu/TodoItem.swift
@@ -0,0 +1,14 @@
+//
+//  TodoItem.swift
+//  Tudu
+//
+//  Created by Ruben de Koning on 16/09/2024.
+//
+
+import Foundation
+
+struct TodoItem: Identifiable {
+    var id = UUID()
+    var title: String
+    var isCompleted: Bool = false
+}
